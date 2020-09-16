ab9b32bcd64322eb7675f88377f2659c8ce226de
LuoJianwei <luojianwei@pinming.cn>
2020-09-16 21:21:28 +0800

    .

--- src/test/java/com/example/spring/flux/demo/WebClientTest.java
+++ src/test/java/com/example/spring/flux/demo/WebClientTest.java
@@ -1,9 +1,14 @@
 package com.example.spring.flux.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class WebClientTest {
@@ -16,5 +21,17 @@ public class WebClientTest {
         stringMono.subscribe(s-> System.err.println(Thread.currentThread().getName() + ">>>" + s));
         System.err.println(Thread.currentThread().getName() + " end..");
         TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Test
+    public void test1(){
+        WebClient.Builder builder = WebClient.builder();
+        builder.uriBuilderFactory(null);
+        builder.defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        builder.defaultCookie("session-id", "123");
+        builder.defaultRequest(r->{});
+
+
+        WebClient webClient = builder.build();
     }
 }
